4da0298cddea6a28473015d56e35d31ed4f5d37e
Marcel Müller <firebird10@gmx.de>
2021-04-10 15:53:52 +0200

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+/** @jsxImportSource @emotion/react */
+import tw, { css } from 'twin.macro'
+
+const App = () => {
+  return (
+    <div>
+      <div>app</div>
+    </div>
+  )
+}
+
+export default App
