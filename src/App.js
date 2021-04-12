14345f340b404e748ee216a67e657ec2d482b982
Marcel Müller <firebird10@gmx.de>
2021-04-12 20:57:02 +0200

    added fullscreen functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useContext, useState, useEffect } from 'react'
+import { useFullScreen } from 'react-browser-hooks'
 
 import { ReactComponent as TickIcon } from './assets/mdi_check-circle-outline.svg'
 import { ReactComponent as CrossIcon } from './assets/mdi_close-circle-outline.svg'
 import { ReactComponent as PersonIcon } from './assets/mdi_account.svg'
 import { ReactComponent as FullscreenIcon } from './assets/mdi_fullscreen.svg'
+import { ReactComponent as ExitFullscreenIcon } from './assets/mdi_fullscreen-exit.svg'
 
 import { PersonCountContext } from './contexts/personCountContext'
 
@@ -13,8 +15,20 @@ import DebugWidget from './components/DebugWidget'
 import tw, { css } from 'twin.macro'
 
 const App = () => {
-  const { personCountState } = useContext(PersonCountContext)
   const [storeFull, setStoreFull] = useState(false)
+  const [menuHidden, setMenuHidden] = useState(false)
+
+  const { personCountState } = useContext(PersonCountContext)
+  const { toggle, fullScreen } = useFullScreen()
+
+  const handleMouseMove = () => {
+    if (menuHidden) {
+      setMenuHidden(false)
+      setTimeout(() => {
+        setMenuHidden(true)
+      }, 7000)
+    }
+  }
 
   useEffect(() => {
     if (personCountState.currentCount >= personCountState.maxCount) {
@@ -22,7 +36,7 @@ const App = () => {
     } else {
       setStoreFull(false)
     }
-  }, [personCountState])
+  }, [personCountState, menuHidden])
 
   return (
     <>
@@ -31,16 +45,22 @@ const App = () => {
           tw`fixed top-0 left-0 bottom-0 right-0 flex flex-col transition-all`,
           storeFull ? tw`bg-ampel-red` : tw`bg-ampel-green`,
         ]}
+        onMouseMove={handleMouseMove}
       >
         <main tw='flex flex-grow '>
-          <div tw='absolute right-5 top-5 text-white z-10 flex space-x-5 items-center'>
+          <div
+            css={[
+              tw`absolute right-5 top-5 text-white z-10 flex space-x-5 items-center`,
+              menuHidden && tw`hidden`,
+            ]}
+          >
             <DebugWidget />
-            <button tw='focus:outline-none hover:(bg-black bg-opacity-30) rounded'>
-              <FullscreenIcon />
+            <button tw='focus:outline-none hover:(bg-black bg-opacity-30) rounded' onClick={toggle}>
+              {fullScreen ? <ExitFullscreenIcon /> : <FullscreenIcon />}
             </button>
           </div>
-          <div tw='w-7/12 flex items-center'>
-            <h1 tw='text-8xl font-bold ml-20'>
+          <div tw='w-7/12 flex items-center p-32'>
+            <h1 tw='text-8xl font-bold'>
               {storeFull
                 ? 'Bitte betreten Sie die Filiale erst, wenn der Bildschirm grün wird.'
                 : 'Sie dürfen die Filiale betreten!'}
